65cb7168f4f1262cfc6430495e436b13bdfe8eef
Sebastián López <sebas.lopezm24@gmail.com>
2020-07-16 19:19:02 -0500

    Updates

---
+++ precache-manifest.34019da24ed3847958723827f3715917.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46686c6ec26ec43d57138b7c40e214b",
+    "url": "/web-development-frontend-course/index.html"
+  },
+  {
+    "revision": "508cafd46cf8310988c0",
+    "url": "/web-development-frontend-course/static/css/main.9519d7e7.chunk.css"
+  },
+  {
+    "revision": "d724350fa0d796e8a5d9",
+    "url": "/web-development-frontend-course/static/js/2.767ea1ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-development-frontend-course/static/js/2.767ea1ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508cafd46cf8310988c0",
+    "url": "/web-development-frontend-course/static/js/main.1a624666.chunk.js"
+  },
+  {
+    "revision": "cc08f8e1d453c3e289d4",
+    "url": "/web-development-frontend-course/static/js/runtime-main.498a8c7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-development-frontend-course/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b5d28b7ecacccffb45c29e0a34dcfaec",
+    "url": "/web-development-frontend-course/static/media/trinity.b5d28b7e.png"
+  }
+]);
